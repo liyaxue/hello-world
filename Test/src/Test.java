d41c7de39a12745c2c3ba69ad7612b9b81b83935
liyaxue <li_ya_xue@126.com>
2016-11-06 20:53:05 +0800

    提交

---
+++ Test/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/10/31.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("hello word");
+        System.out.println("");
+
+    }
+}
